c28d38ec95cb18ba932af3dbe40f76c8f9c107ff
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-09 08:51:29 +0530

    Create Number Guessing Game using while loop.py

---
+++ Number Guessing Game using while loop.py
@@ -0,0 +1,10 @@
+import random
+target = random.randint(1, 100)
+guess = None
+while guess != target:
+    guess = int(input("Guess the number (1-100): "))
+    if guess < target:
+        print("Too low")
+    elif guess > target:
+        print("Too high")
+print("Correct! You guessed it.")
