4f833175fc04ae1fc712edac918ccc130ef4b963
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-09 08:17:00 +0530

    Create Simple Login System.py

---
+++ Simple Login System.py
@@ -0,0 +1,6 @@
+username = input("Username: ")
+password = input("Password: ")
+if username == "admin" and password == "pass123":
+    print("Login successful")
+else:
+    print("Invalid credentials")
